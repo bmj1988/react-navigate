0efa56fc7ad87c723fda84a68c5ed5d365b378ae
Brian <cjcores@gmail.com>
2023-11-08 19:38:46 -0500

    finished navigate for react

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,57 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: 'stocks',
-    element: <Stocks />
-  },
-  {
-    path: 'movies',
-    element: <Movies />
-  },
-  {
-    path: '*',
-    element: <h1>Page Not Found</h1>
+    element: <Layout />,
+    children: [
+      {
+        path: '/not-logged-in',
+        element: <h1>You Must Be Logged In To Enter.</h1>
+      },
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'stocks',
+        element: <Stocks />
+      },
+      {
+        path: 'movies',
+        element: <Movies />
+      },
+      {
+        path: '*',
+        element: <h1>Page Not Found</h1>
+      }]
   }
 ]);
-
-function App() {
+function Layout() {
   return (
     <div className='app'>
       <h1>App Component</h1>
-      <RouterProvider router={router} />
+      <nav className='comp nav'>
+        <ul>
+          <li>
+            <NavLink to='/' className={({ isActive }) => isActive ? 'purple' : ''} style={({ isActive }) => isActive ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/stocks' className={({ isActive }) => isActive ? 'purple' : ''} style={({ isActive }) => isActive ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Stocks</NavLink>
+          </li>
+          <li><NavLink to='/movies' className={({ isActive }) => isActive ? 'purple' : ''} style={({ isActive }) => isActive ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Movies</NavLink></li>
+        </ul>
+      </nav>
+      <main><Outlet /></main>
     </div>
+  )
+}
+function App() {
+  return (
+
+    <RouterProvider router={router} />
   );
 }
 
--- src/components/Stocks/Stocks.jsx
+++ src/components/Stocks/Stocks.jsx
@@ -1,7 +1,26 @@
+import {Navigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
 function Stocks() {
-  return (
+  const navigate = useNavigate()
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+    navigate('/');
+  };
+  let loggedIn = true
+  if (loggedIn === false) {
+    return (
+      <Navigate to='/not-logged-in' replace={true}></Navigate>
+    )
+  }
+  else return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Button</button>
     </div>
   );
 }
